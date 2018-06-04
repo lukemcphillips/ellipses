597e440263acd003a60e9bf5f334d52654717ece
lukemcphillips <lrmcphillips@gmail.com>
2018-06-04 10:02:55 -0400

    Initial Commit

---
+++ R/perimeter.R
@@ -0,0 +1,20 @@
+#' Calculate the length of the perimeter of an ellipse.
+#'
+#' @param r Length of the semi-major axis.
+#' @param e The eccentricity.
+#' @return The length of the perimeter of the ellipse with specified semi-major axis and eccentricity.
+#' @examples
+#' ellipse_perimeter(1, 1)
+#' ellipse_perimeter(1)
+#' @export
+#' @importFrom pracma ellipke
+
+ellipse_perimeter <- function(r, e = 0) {
+  if (r <= 0) stop ("Invalid semi-major axis", call. = FALSE)
+  if (e < 0 || e > 1) stop("Invalid eccentricity!", call. = FALSE)
+  if (e == 0) {
+    2 * pi * r
+  } else {
+    4 * r * pracma::ellipke(e)$e
+  }
+}
